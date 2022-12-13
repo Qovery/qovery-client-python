8738d05881bb8210f07e60961b5afd1adb90128d
qovery <github@qovery.com>
2022-12-13 16:15:41 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/f2b047d5ed66d72ea1bb767608adef6885a7abd5 from refs/heads/main

--- qovery/model/api_variable_scope_enum.py
+++ qovery/model/api_variable_scope_enum.py
@@ -58,6 +58,7 @@ class APIVariableScopeEnum(ModelSimple):
             'ENVIRONMENT': "ENVIRONMENT",
             'PROJECT': "PROJECT",
             'CONTAINER': "CONTAINER",
+            'JOB': "JOB",
         },
     }
 
@@ -109,10 +110,10 @@ class APIVariableScopeEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", ]  # noqa: E501
+            args[0] (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", "JOB", ]  # noqa: E501
 
         Keyword Args:
-            value (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", ]  # noqa: E501
+            value (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", "JOB", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -203,10 +204,10 @@ class APIVariableScopeEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", ]  # noqa: E501
+            args[0] (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", "JOB", ]  # noqa: E501
 
         Keyword Args:
-            value (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", ]  # noqa: E501
+            value (str):, must be one of ["APPLICATION", "BUILT_IN", "ENVIRONMENT", "PROJECT", "CONTAINER", "JOB", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
