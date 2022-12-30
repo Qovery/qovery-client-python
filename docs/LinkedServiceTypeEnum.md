0bb8fb6610a52793f3de7ac79a2d68eb73802831
qovery <github@qovery.com>
2022-12-30 14:51:14 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/bee986ab862acf4f085a2e0070b98728f9b0f951 from refs/heads/main

--- docs/LinkedServiceTypeEnum.md
+++ docs/LinkedServiceTypeEnum.md
@@ -5,7 +5,7 @@ type of the service (application, database, job, gateway...)   present only for 
 ## Properties
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**value** | **str** | type of the service (application, database, job, gateway...)   present only for &#x60;BUILT_IN&#x60; variable  |  must be one of ["APPLICATION", "CONTAINER", "DATABASE", ]
+**value** | **str** | type of the service (application, database, job, gateway...)   present only for &#x60;BUILT_IN&#x60; variable  |  must be one of ["APPLICATION", "CONTAINER", "DATABASE", "JOB", ]
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- qovery/model/linked_service_type_enum.py
+++ qovery/model/linked_service_type_enum.py
@@ -56,6 +56,7 @@ class LinkedServiceTypeEnum(ModelSimple):
             'APPLICATION': "APPLICATION",
             'CONTAINER': "CONTAINER",
             'DATABASE': "DATABASE",
+            'JOB': "JOB",
         },
     }
 
@@ -107,10 +108,10 @@ class LinkedServiceTypeEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", ]  # noqa: E501
+            args[0] (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", "JOB", ]  # noqa: E501
 
         Keyword Args:
-            value (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", ]  # noqa: E501
+            value (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", "JOB", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -201,10 +202,10 @@ class LinkedServiceTypeEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", ]  # noqa: E501
+            args[0] (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", "JOB", ]  # noqa: E501
 
         Keyword Args:
-            value (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", ]  # noqa: E501
+            value (str): type of the service (application, database, job, gateway...)   present only for `BUILT_IN` variable ., must be one of ["APPLICATION", "CONTAINER", "DATABASE", "JOB", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
