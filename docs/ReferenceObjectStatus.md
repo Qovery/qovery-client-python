44e70e1437865e5b93c15693ffc981c6894161ef
qovery <github@qovery.com>
2023-04-14 10:27:22 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/933b31f78bbb725fe66a6c84f2246c138599175f from refs/heads/main

--- docs/ReferenceObjectStatus.md
+++ docs/ReferenceObjectStatus.md
@@ -8,6 +8,7 @@ Name | Type | Description | Notes
 **state** | [**StateEnum**](StateEnum.md) |  | 
 **service_deployment_status** | [**ServiceDeploymentStatusEnum**](ServiceDeploymentStatusEnum.md) |  | 
 **last_deployment_date** | **datetime** |  | [optional] 
+**is_part_last_deployment** | **bool** |  | [optional] 
 **any string name** | **bool, date, datetime, dict, float, int, list, str, none_type** | any string name can be used but the value must be the correct type | [optional]
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- qovery/model/reference_object_status.py
+++ qovery/model/reference_object_status.py
@@ -98,6 +98,7 @@ class ReferenceObjectStatus(ModelComposed):
             'state': (StateEnum,),  # noqa: E501
             'service_deployment_status': (ServiceDeploymentStatusEnum,),  # noqa: E501
             'last_deployment_date': (datetime,),  # noqa: E501
+            'is_part_last_deployment': (bool,),  # noqa: E501
         }
 
     @cached_property
@@ -110,6 +111,7 @@ class ReferenceObjectStatus(ModelComposed):
         'state': 'state',  # noqa: E501
         'service_deployment_status': 'service_deployment_status',  # noqa: E501
         'last_deployment_date': 'last_deployment_date',  # noqa: E501
+        'is_part_last_deployment': 'is_part_last_deployment',  # noqa: E501
     }
 
     read_only_vars = {
@@ -155,6 +157,7 @@ class ReferenceObjectStatus(ModelComposed):
                                 through its discriminator because we passed in
                                 _visited_composed_classes = (Animal,)
             last_deployment_date (datetime): [optional]  # noqa: E501
+            is_part_last_deployment (bool): [optional]  # noqa: E501
         """
 
         _check_type = kwargs.pop('_check_type', True)
@@ -262,6 +265,7 @@ class ReferenceObjectStatus(ModelComposed):
                                 through its discriminator because we passed in
                                 _visited_composed_classes = (Animal,)
             last_deployment_date (datetime): [optional]  # noqa: E501
+            is_part_last_deployment (bool): [optional]  # noqa: E501
         """
 
         _check_type = kwargs.pop('_check_type', True)
