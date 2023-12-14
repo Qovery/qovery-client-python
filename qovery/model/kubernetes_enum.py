2a91ed952a52f186c63fad2d5c078013a1114102
qovery <github@qovery.com>
2023-12-14 10:21:12 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/89eb7831493cb060b42062bfb83904e958ec2fda from refs/heads/main

--- qovery/model/kubernetes_enum.py
+++ qovery/model/kubernetes_enum.py
@@ -55,6 +55,7 @@ class KubernetesEnum(ModelSimple):
         ('value',): {
             'K3S': "K3S",
             'MANAGED': "MANAGED",
+            'SELF_MANAGED': "SELF_MANAGED",
         },
     }
 
@@ -106,10 +107,10 @@ class KubernetesEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", ]  # noqa: E501
+            args[0] (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", "SELF_MANAGED", ]  # noqa: E501
 
         Keyword Args:
-            value (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", ]  # noqa: E501
+            value (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", "SELF_MANAGED", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -196,10 +197,10 @@ class KubernetesEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", ]  # noqa: E501
+            args[0] (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", "SELF_MANAGED", ]  # noqa: E501
 
         Keyword Args:
-            value (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", ]  # noqa: E501
+            value (str): if omitted defaults to "MANAGED", must be one of ["K3S", "MANAGED", "SELF_MANAGED", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
