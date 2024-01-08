ec4c65501cba4bfd297292f8d709f0ead4d90332
qovery <github@qovery.com>
2024-01-08 14:04:26 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/8e8a804d251b7f9fbc5a15bbb1554167ee298dd3 from refs/heads/main

--- qovery/model/container_registry_kind_enum.py
+++ qovery/model/container_registry_kind_enum.py
@@ -59,7 +59,6 @@ class ContainerRegistryKindEnum(ModelSimple):
             'GITHUB_CR': "GITHUB_CR",
             'GITLAB_CR': "GITLAB_CR",
             'PUBLIC_ECR': "PUBLIC_ECR",
-            'DOCR': "DOCR",
             'GENERIC_CR': "GENERIC_CR",
             'GCP_ARTIFACT_REGISTRY': "GCP_ARTIFACT_REGISTRY",
         },
@@ -113,10 +112,10 @@ class ContainerRegistryKindEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "DOCR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
+            args[0] (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
 
         Keyword Args:
-            value (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "DOCR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
+            value (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -207,10 +206,10 @@ class ContainerRegistryKindEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "DOCR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
+            args[0] (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
 
         Keyword Args:
-            value (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "DOCR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
+            value (str): The type of your container registry., must be one of ["ECR", "SCALEWAY_CR", "DOCKER_HUB", "GITHUB_CR", "GITLAB_CR", "PUBLIC_ECR", "GENERIC_CR", "GCP_ARTIFACT_REGISTRY", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
