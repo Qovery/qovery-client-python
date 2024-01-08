8fb35d45daacd59d182a772e6d60feca7d758b1f
qovery <github@qovery.com>
2024-01-08 13:50:56 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/fd69158dfcb6859493038a788259f2d0b0bb105b from refs/heads/main

--- qovery/model/cloud_provider_enum.py
+++ qovery/model/cloud_provider_enum.py
@@ -54,7 +54,6 @@ class CloudProviderEnum(ModelSimple):
     allowed_values = {
         ('value',): {
             'AWS': "AWS",
-            'DO': "DO",
             'SCW': "SCW",
             'GCP': "GCP",
         },
@@ -108,10 +107,10 @@ class CloudProviderEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str):, must be one of ["AWS", "DO", "SCW", "GCP", ]  # noqa: E501
+            args[0] (str):, must be one of ["AWS", "SCW", "GCP", ]  # noqa: E501
 
         Keyword Args:
-            value (str):, must be one of ["AWS", "DO", "SCW", "GCP", ]  # noqa: E501
+            value (str):, must be one of ["AWS", "SCW", "GCP", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
@@ -202,10 +201,10 @@ class CloudProviderEnum(ModelSimple):
         Note that value can be passed either in args or in kwargs, but not in both.
 
         Args:
-            args[0] (str):, must be one of ["AWS", "DO", "SCW", "GCP", ]  # noqa: E501
+            args[0] (str):, must be one of ["AWS", "SCW", "GCP", ]  # noqa: E501
 
         Keyword Args:
-            value (str):, must be one of ["AWS", "DO", "SCW", "GCP", ]  # noqa: E501
+            value (str):, must be one of ["AWS", "SCW", "GCP", ]  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
                                 raised if the wrong type is input.
